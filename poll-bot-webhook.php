a2d55502a1503e3a31154cce9fa784d852a73d80
facebokiii <botisheree@gmail.com>
2015-12-23 22:22:34 +0330

    Update poll-bot-webhook.php

--- poll-bot-webhook.php
+++ poll-bot-webhook.php
@@ -2,7 +2,7 @@
 
 require_once 'PollBot.php';
 
-define('BOT_TOKEN', 'XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+define('BOT_TOKEN', '135932930:AAFiFWjCvvdN0BwOYvaXt5oStodBFwEqc6I');
 define('BOT_WEBHOOK', 'https://bot.server/poll-bot-webhook.php');
 
 $bot = new PollBot(BOT_TOKEN, 'PollBotChat');
