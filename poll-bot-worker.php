9da1c94d7d1da4812472d167393763e06ded55e9
facebokiii <botisheree@gmail.com>
2015-12-23 22:22:20 +0330

    Update poll-bot-worker.php

--- poll-bot-worker.php
+++ poll-bot-worker.php
@@ -4,7 +4,7 @@ set_time_limit(0);
 
 require_once 'PollBot.php';
 
-define('BOT_TOKEN', 'XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+define('BOT_TOKEN', '135932930:AAFiFWjCvvdN0BwOYvaXt5oStodBFwEqc6I');
 
 $bot = new PollBot(BOT_TOKEN, 'PollBotChat');
 $bot->runLongpoll();
